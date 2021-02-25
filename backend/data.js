636568657250b46a9c2f15436edbbce6bdd795fd
atiwat02 <atiwat.ka.61@ubu.ac.th>
2021-02-25 17:21:31 +0700

    first commit

---
+++ backend/data.js
@@ -0,0 +1,28 @@
+module.exports = {
+    products:[
+        {
+            _id: '1',
+            nameStore: 'Vintageshop',
+            price:'150',
+            number:'200',
+            image:'https://obs.line-scdn.net/0h0jSQ6uHHb0VuKEA7ZLMQElR-bCpdRHxGCh4-RjJGMXFLGCASVBxyK00uM3MRSigbB08kI0ktdHREHCwQBh1y/w644',
+            brand: 'Vintage',
+        },
+        {
+            _id: '2',
+            nameStore: 'Piemier',
+            price:'100',
+            number:'1000',
+            image:'https://cf.shopee.co.th/file/7e4d48de1b609c2aed53fcbb501d0212', 
+            brand: 'Nike', 
+        },
+        {
+            _id: '3',
+            nameStore: 'Nana Shoes Shop',
+            price:'1559',
+            number:'500',
+            image:'https://promotions.co.th/wp-content/uploads/2020/02/%E0%B8%A3%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%97%E0%B9%89%E0%B8%B2%E0%B8%9C%E0%B9%89%E0%B8%B2%E0%B9%83%E0%B8%9A-Adidas-Superstar-Courtside-Collection.jpg',
+            brand: 'adidas',
+        },
+    ]
+}
